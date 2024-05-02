c27f0200da7a42c1bace21199c4557fb198268a3
SEUNGJUN JEONG <seungjun.jeong@studentambassadors.com>
2024-05-02 19:53:23 +0900

    Title: 격자 반대로 채우기, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240502/격자 반대로 채우기/grid-anti-fill.py
@@ -0,0 +1,20 @@
+n = int(input())
+
+arr = [[1 for _ in range(n)] for _ in range(n)]
+
+count = 0
+
+for i in range(n-1, -1, -1):
+    if i % 2 != 0:
+        for j in range(n-1, -1, -1):
+            arr[j][i] += count
+            count += 1
+    else:
+        for j in range(n):
+            arr[j][i] += count
+            count += 1
+
+for i in range(n):
+    for j in range(n):
+        print(arr[i][j], end=" ")
+    print()
