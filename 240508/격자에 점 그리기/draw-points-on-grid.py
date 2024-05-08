99e0ca052ccc6adeac27bcd8d80dcb9d96354efb
SEUNGJUN JEONG <seungjun.jeong@studentambassadors.com>
2024-05-08 21:48:37 +0900

    Title: 격자에 점 그리기, Time: 139ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240508/격자에 점 그리기/draw-points-on-grid.py
@@ -0,0 +1,18 @@
+n, m = map(int, input().split())
+
+arr = [
+    [0 for _ in range(n)]
+    for _ in range(n)
+]
+
+count = 1
+
+for i in range(m):
+    r, c = map(int, input().split())
+    arr[r-1][c-1] += count
+    count += 1
+
+for i in range(n):
+    for j in range(n):
+        print(arr[i][j], end=" ")
+    print()
